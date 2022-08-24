dfa6c3654d68f1e5f42ac28f10bf6e72b443e752
Seungyeon Lee <39074545+yeon-lab@users.noreply.github.com>
2022-08-24 15:08:58 -0400

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,210 @@
+import pandas as pd
+import numpy as np
+import argparse
+from tqdm import tqdm
+import os 
+
+    
+def load_dataset(path='./data/'):
+    outpatient_files = os.listdir(path)
+
+    files = [os.path.join(path, file) for file in outpatient_files]
+    
+    data = pd.DataFrame()
+    for file in files:
+        df = pd.read_csv(file, dtype=str)
+        df = df[~df['DX1'].isnull()]
+        df = df.drop(labels=['PROCTYP','PROC1'],axis=1)
+        df = df.drop_duplicates()
+        data = pd.concat([data,df], axis=0)
+        
+    data = data.replace({'DXVER': np.nan}, {'DXVER': '9'})
+    data.SVCDATE = pd.to_datetime(data.SVCDATE)
+    data.sort_values(by=['ENROLID', 'SVCDATE'], inplace=True)
+    data = data.reset_index(drop=True)
+    
+    return data
+    
+    
+
+def icd9_to_icd10(dx, mapping):
+    if '.' in dx:
+        dx = dx[:-2]  
+    
+    if dx in mapping.keys():
+        dx = dx
+    elif '0'+dx in mapping.keys():
+        dx = '0'+dx 
+    elif '00'+dx in mapping.keys():
+        dx = '00'+dx
+    elif '000'+dx in mapping.keys():
+        dx = '000'+dx
+    else:
+        print(dx,'not in dict')
+        return None
+        
+    return mapping[dx]
+
+def map_function(x_dxs, mapping):
+    map_list = list()
+    for x_dx in x_dxs:
+        mapped = icd9_to_icd10(x_dx, mapping)
+        if mapped is not None:
+            map_list.append(mapped)
+            
+    return map_list
+
+def get_dx_list(patient_x, X_DXVER, mapping, DX_col):
+    X = list()
+    for date in patient_x.SVCDATE.unique():
+        record = patient_x[patient_x.SVCDATE == date]
+        if len(record) == 1:
+            x_dxs = sum(record[DX_col].dropna(axis=1).values.tolist(), [])
+
+            if X_DXVER == '0':
+                X.append(x_dxs)
+            else:
+                x_dxs_mapped = map_function(x_dxs, mapping)
+                if len(x_dxs_mapped) != 0:
+                    X.append(x_dxs_mapped)
+        else:
+            x_dxs = sum(record[DX_col].values.tolist(), []) 
+            x_dxs = list(map(str, list(set(x_dxs))))
+            x_dxs = [x for x in x_dxs if x != 'nan']
+
+            if X_DXVER == '0':
+                X.append(x_dxs)
+            else:
+                x_dxs_mapped = map_function(x_dxs, mapping)
+                if len(x_dxs_mapped) != 0:
+                    X.append(x_dxs_mapped)    
+                    
+    return X
+    
+def check_target(dx, target_dict):
+    for key in target_dict.keys():
+        for code in target_dict[key]:
+            if dx.startswith(code):
+                return True, key
+    return False, None
+    
+
+def get_cases(df, batch_ids, target_dict, mapping, config):
+    n_visits = config.n_visits
+
+    DX_col = [col for col in df.columns if 'DX' in col and col != 'DXVER']
+    
+    case_group = {
+        'ID':[],
+        'DXVER': [],
+        'X': [],
+        'Y': [],
+        'X_idx': [],
+        'Y_idx': []
+    }
+    control_group_ids = []        
+    for id_ in tqdm(batch_ids, total=len(batch_ids)):
+        is_saved = False
+        patient = df[df.ENROLID == id_].reset_index(drop=True)
+        if len(patient) < n_visits:
+            continue
+        if (patient.SVCDATE[-1:].item()-patient.SVCDATE[n_visits-1]).days < 365:
+            continue
+            
+        for i, (_, row) in enumerate(patient.iterrows()):
+            DXVER = '9'
+            if not pd.isnull(row['DXVER']):
+                DXVER = row['DXVER']
+    
+            dxs = list(row[DX_col])
+            for dx in dxs:
+                if not pd.isnull(dx):
+                    is_case, target = check_target(dx, target_dict[DXVER])
+    
+            if is_case and i < n_visits:
+                break
+            if is_case and i >= n_visits:
+                if (row['SVCDATE']-patient.SVCDATE[n_visits-1]).days < 365:
+                    break
+                for j in range(i-1, n_visits-2, -1):
+                    if (row['SVCDATE']-patient.SVCDATE[j]).days > 365:
+                        patient_ = patient.iloc[:j+1,:]
+                        for X_DXVER in ['9','0']:
+                            patient_x = patient_[patient_.DXVER==X_DXVER]
+                            if len(patient_x.SVCDATE.unique()) >= n_visits:
+                                X = get_dx_list(patient_x, X_DXVER, mapping, DX_col)
+                                if len(X) >= n_visits:
+                                    is_saved = True
+                                    Y = target
+                                    X_idx = j
+                                    Y_idx = i
+                                    case_group['ID'].append(id_)
+                                    case_group['DXVER'].append(X_DXVER)
+                                    case_group['X'].append(X)
+                                    case_group['Y'].append(Y)
+                                    case_group['X_idx'].append(X_idx)
+                                    case_group['Y_idx'].append(Y_idx)
+
+                    if is_saved:
+                        break
+                break
+    
+
+        if not is_case:
+            control_group_ids.append(id_)
+            
+        
+    np.save(os.path.join(config.save_dir, 'control_group_ids_{}'.format(config.fold_id)), control_group_ids)  
+    np.save(os.path.join(config.save_dir, 'case_group_{}'.format(config.fold_id)), case_group) 
+    
+    
+
+        
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument('--path', default = './data/',type=str)
+    args.add_argument('--save_dir', default = './data_npy/',type=str)
+    args.add_argument('--n_visits', default = 10,type=int)
+    args.add_argument('--n_split', type=int)
+    args.add_argument('--fold_id', type=int)
+    
+    config = args.parse_args()
+    
+    target_dict = {
+        '9': {
+            'HF': ['425', '428', '40201', '40211', '40291', '40401', '40403', '40411', '40413', '40491', '40493', 'K77'],
+            'AF':['42731'],
+            'CAD':['4111','41181','41189','4130','4131','4139','412','41400','41401','4142','4143','4148','4149',
+                        '4292','41402','41403','41404','41405','V4581','V4582'],     ###
+            'PAD':['4439','4438','44020','44021'],
+            'ST': ['4380', '4381', '4382', '4383', '4384', '4385', '4386', '4387', '4388', '4389', 'V1254'],
+            'HT': ['401','403','405']  ###
+            
+        },
+        '0':{
+            'HF': ['I11', 'I13', 'I50', 'I42', 'K77'],
+            'AF': ['I480','I481','I482','I4891'],
+            'CAD': ['I20', 'I240', 'I248','I25'],
+            'PAD': ['I73'],
+            'ST': ['Z8673', 'I60', 'I61', 'I62', 'I63', 'I64', 'I65', 'I66', 'I67', 'I68', 'I69', 'G458', 'G459'],
+            'HT': ['I10','I12','I15','I16'] ###
+        }
+    }
+    
+    mapping = pd.read_csv('icd9to10.csv')[['icd9cm', 'icd10cm']]
+    mapping = dict(mapping.values.tolist())
+        
+    df = load_dataset(config.path)
+
+    all_ids = df.ENROLID.unique()
+    batch_size = len(all_ids)//config.n_split
+    
+    if config.fold_id == config.n_split-1:
+        batch_ids= all_ids[config.fold_id*batch_size:]
+    else:
+        batch_ids= all_ids[config.fold_id*batch_size : (1+config.fold_id)*batch_size]
+
+    print(f'all unique id: {len(all_ids)}, n split: {config.n_split}, batch size: {batch_size}, batch size: {len(batch_ids)}')
+    
+    get_cases(df, batch_ids, target_dict, mapping, config)
+ 
---
+++ train_dipole.py
@@ -0,0 +1,101 @@
+import argparse
+import collections
+import numpy as np
+
+import model.loss as module_loss
+import model.metric as module_metric
+from parse_config import ConfigParser
+from trainer import Trainer
+from utils.util import *
+from model.Dipole import *
+
+import torch
+import torch.nn as nn
+
+
+# fix random seeds for reproducibility
+SEED = 1111
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = False
+
+def weights_init_normal(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.uniform_(m.weight.data, -0.1, 0.1)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+    elif type(m) == nn.GRU:
+        for layer_p in m._all_weights:
+            for p in layer_p:
+                if 'weight' in p:
+                    torch.nn.init.uniform_(m.__getattr__(p), -0.1, 0.1)
+                if 'bias' in p:
+                    torch.nn.init.constant_(m.__getattr__(p), 0.0)
+                    
+def main(config, dataset):
+    logger = config.get_logger('train') 
+    logger.info('='*100)
+    for key, value in config['hyper_params'].items():
+        logger.info('    {:25s}: {}'.format(str(key), value))
+    for key, value in config['data_loader']['args'].items():
+        logger.info('    {:25s}: {}'.format(str(key), value))
+    logger.info('    {:25s}: {}'.format('optimizer', config['optimizer']['type']))
+    for key, value in config['optimizer']['args'].items():
+        logger.info('    {:25s}: {}'.format(str(key), value))
+    for key, value in config['trainer'].items():
+        logger.info('    {:25s}: {}'.format(str(key), value))
+
+    
+    logger.info("="*100)
+        
+    
+    # build model architecture, initialize weights, then print to console  
+    
+    model = Dipole(config=config)
+    model.apply(weights_init_normal)
+
+    logger.info(model)
+    logger.info("-"*100)
+
+
+    # get function handles of loss and metrics
+    criterion = getattr(module_loss, config['loss'])
+    metrics = [getattr(module_metric, met) for met in config['metrics']]
+
+    # build optimizer
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = config.init_obj('optimizer', torch.optim, model_parameters)
+
+    trainer = Trainer(model, 
+                      optimizer,
+                      criterion, 
+                      metrics,
+                      config=config,
+                      train_data=dataset['train_case'],
+                      valid_data=dataset['valid_case'],
+                      test_data_ICD9=dataset['test_case_ICD9'],
+                      test_data_ICD10=dataset['test_case_ICD10']
+                      )
+
+    trainer.train()
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PyTorch Template')
+    args.add_argument('-c', '--config', type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-d', '--device', default="0", type=str,
+                      help='indices of GPUs to enable (default: all)')
+    args.add_argument('-da', '--np_data_dir', default='./data_npy/', type=str,
+                      help='Directory containing numpy files')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to latest checkpoint (default: None)')
+
+    CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
+
+    args2 = args.parse_args()
+
+    config = ConfigParser.from_args(args)
+    dataset, feat_dict, y_dict = init_data(args2.np_data_dir)
+    config['hyper_params']['n_feat'], config['hyper_params']['n_class'] = len(feat_dict.keys()), len(y_dict.keys())
+
+    main(config, dataset)
