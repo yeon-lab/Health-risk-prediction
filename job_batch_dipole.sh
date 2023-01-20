47602d0af3d04acee16f03dd56e65b89224f7d34
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-01-19 19:16:36 -0500

    Update job_batch_dipole.sh

--- job_batch_dipole.sh
+++ job_batch_dipole.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#SBATCH --time=10:00:00
 #SBATCH --nodes=1 --ntasks-per-node=8 --gpus-per-node=1 --gpu_cmode=shared
-#SBATCH --job-name=dipole
-#SBATCH --account=PCON0023
-#SBATCH --exclude=p0240
 
 module load python
 source activate local
 
-python train_dipole.py --config 'config_dipole.json'
+python train.py --config 'config.json'
