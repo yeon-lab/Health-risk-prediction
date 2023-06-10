b89d9c95dcf74ca8ace94447e4ae54fb7f2a51be
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-06-09 23:25:34 -0400

    Update metric.py

--- model/metric.py
+++ model/metric.py
@@ -1,39 +1,35 @@
 import torch
-from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+from sklearn.metrics import precision_recall_curve, auc
 from torchmetrics import AUROC
 
-
-
-def accuracy(target, output, out_dim):
+def accuracy(target, output):
     target = target.cpu().numpy().reshape(-1,1)
-    if out_dim == 1:
-        output = torch.where(output > 0.5, 1, 0).cpu().numpy().reshape(-1,1)
-    else:
-        output = output.data.max(1, keepdim=True)[1].cpu().numpy().reshape(-1,1)
+    output = torch.where(output > 0.5, 1, 0).cpu().numpy().reshape(-1,1)
     return accuracy_score(target, output)
 
-    
-def roc_auc(target, output, out_dim):
+def auroc(target, output):
     target = target.long()
-    if out_dim == 1:
-        auroc = AUROC(pos_label=1)
-    else:
-        auroc = AUROC(num_classes = out_dim)
-    return auroc(output, target).cpu().numpy()
-
-def f1(target, output, out_dim):
-    target = target.cpu().numpy().reshape(-1,1)
-    if out_dim == 1:
-        output = torch.where(output > 0.5, 1, 0).cpu().numpy().reshape(-1,1)
-    else:
-        output = output.data.max(1, keepdim=True)[1].cpu().numpy().reshape(-1,1)
-    return f1_score(target, output, average='macro')
+    auroc_func = AUROC(task="binary")
+    return auroc_func(output, target).cpu().numpy()
     
-    
-def confusion(target, output, out_dim):
+def auprc(target, output):
     target = target.cpu().numpy().reshape(-1,1)
-    if out_dim == 1:
-        output = torch.where(output > 0.5, 1, 0).cpu().numpy().reshape(-1,1)
-    else:
-        output = output.data.max(1, keepdim=True)[1].cpu().numpy().reshape(-1,1)
+    output = output.detach().cpu().numpy().reshape(-1,1)
+    precision, recall, thresholds = precision_recall_curve(target, output)
+    return auc(recall, precision)
+    
+def precision(target, output):
+    target = target.cpu().numpy().reshape(-1,1)
+    output = torch.where(output > 0.5, 1, 0).cpu().numpy().reshape(-1,1)
+    return precision_score(target, output, average='macro')
+    
+def recall(target, output):
+    target = target.cpu().numpy().reshape(-1,1)
+    output = torch.where(output > 0.5, 1, 0).cpu().numpy().reshape(-1,1)
+    return recall_score(target, output, average='macro')
+    
+def confusion(target, output):
+    target = target.cpu().numpy().reshape(-1,1)
+    output = torch.where(output > 0.5, 1, 0).cpu().numpy().reshape(-1,1)
     return confusion_matrix(target,output)
