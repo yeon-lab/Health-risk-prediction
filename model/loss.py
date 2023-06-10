0e4cf3d7c5277f345ec363a2d246aa7b58dd2a04
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-06-09 23:25:49 -0400

    Update loss.py

--- model/loss.py
+++ model/loss.py
@@ -1,12 +1,8 @@
 import torch
 import torch.nn as nn
-
-
-def CrossEntropyLoss(output, target):
-    cr = nn.CrossEntropyLoss()
-    return cr(output, target)
        
 def BCELoss(output, target):
     cr = nn.BCELoss(reduction='none')
     return cr(output, target)       
        
+
